0a2f0a5f8ee7753710e4e3cc6b30f2a0e6634108
exozy <exozy.nebulous@gmail.com>
2024-12-01 11:36:03 +0500

    1 checkpoint

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -1,31 +1,22 @@
 public class SimpleDotCom {
   int[] locationCells;
   int numOfHits = 0;
-
-  // Устанавливаем расположение ячеек
   public void setLocationCells(int[] locs) {
       locationCells = locs;
   }
-
-  // Проверяем ответ пользователя
   public String checkYourself(String stringGuess) {
-      int guess = Integer.parseInt(stringGuess);  // Преобразуем строку в число
-      String result = "мимо";  // Изначально считаем, что попадания нет
-
-      // Перебираем ячейки
+      int guess = Integer.parseInt(stringGuess);  
+      String result = "мимо";  
       for (int cell : locationCells) {
           if (guess == cell) {
               numOfHits++;
-              result = "попал";  // Если угадали
-              break;  // Если угадали, больше не проверяем
+              result = "попал"; 
+              break;  
           }
       }
-
-      // Если все ячейки были угаданы
       if (numOfHits == locationCells.length) {
-          result = "выиграл";  // Если все попадания совершены, игра окончена
+          result = "выиграл";  
       }
-
-      return result;  // Возвращаем результат
+      return result;  
   }
 }
