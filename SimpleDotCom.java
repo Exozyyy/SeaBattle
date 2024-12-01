c9ca64d947c8f08b1db3a2ab418c80c50fdfe0c0
exozy <exozy.nebulous@gmail.com>
2024-12-01 20:00:37 +0500

    final result

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -2,64 +2,73 @@ import java.util.ArrayList;
 
 public class SimpleDotCom {
     int numOfHits = 0;
-    ArrayList<Integer> locationCells;  
-    ArrayList<Integer> guessedCells;   
+    ArrayList<String> locationCells;
+    ArrayList<String> guessedCells;
 
- 
-    public void setLocationCells(ArrayList<Integer> locs) {
+    public void setLocationCells(ArrayList<String> locs) {
         locationCells = locs;
-        guessedCells = new ArrayList<>();  
+        guessedCells = new ArrayList<>();
     }
 
-
-
     public String checkYourself(String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
         String result = "мимо";
         
-        if (!guessedCells.contains(guess)) {
-            guessedCells.add(guess);
-        }
-        else {
+        if (guessedCells.contains(stringGuess)) {
             return "Эта клетка уже была угадана";
+        } else {
+            guessedCells.add(stringGuess);
         }
 
-        if (locationCells.contains(guess)) {
+        if (locationCells.contains(stringGuess)) {
             numOfHits++;
-            int index = locationCells.indexOf(guess);
-            locationCells.remove(index);  
+            locationCells.remove(stringGuess);  // Удаляем угаданную клетку
             result = "попал";
         }
 
         if (locationCells.isEmpty()) {
             result = "потопил";
+
         }
         return result;
     }
 
-
     public static void main(String[] args) {
         GameHelper helper = new GameHelper();
         SimpleDotCom theDotCom = new SimpleDotCom();
-        
-        
-        ArrayList<Integer> locations = new ArrayList<>();
-        int randomNum = (int) (Math.random() * 5);
-        for (int i = randomNum; i < randomNum + 3; i++) {
-            locations.add(i);  
+
+        String[][] playArea = new String[7][7];
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                playArea[i][j] = "[]";
+            }
         }
-        
+
+        ArrayList<String> locations = new ArrayList<>();
+        int randomRow = (int) (Math.random() * 7);
+        int randomColumn = (int) (Math.random() * 5); 
+        for (int i = 0; i < 3; i++) {
+            String cell = randomRow + "," + (randomColumn + i);
+            locations.add(cell);
+            playArea[randomRow][randomColumn + i] = " X"; 
+        }
         theDotCom.setLocationCells(locations);
 
         int numOfGuesses = 0;
         boolean isAlive = true;
 
         while (isAlive) {
-            String guess = helper.getUserInput("введите число");
+            System.out.println("Поле для игры:");
+            for (int i = 0; i < 7; i++) {
+                for (int j = 0; j < 7; j++) {
+                    System.out.print(playArea[i][j]);
+                }
+                System.out.println();
+            }
+            String guess = helper.getUserInput("введите число  ");
             String result = theDotCom.checkYourself(guess);
             numOfGuesses++;
             System.out.println(result);
-            if (result.equals("выиграл")) {
+            if (result.equals("потопил")) {
                 isAlive = false;
                 System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
             }
