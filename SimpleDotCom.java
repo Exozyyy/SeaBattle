b734a5a270f023bf026e03896ead2e8d1d82f6e2
exozy <exozy.nebulous@gmail.com>
2024-12-01 19:01:47 +0500

    3 checkpoint  without error

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -2,42 +2,58 @@ import java.util.ArrayList;
 
 public class SimpleDotCom {
     int numOfHits = 0;
-    ArrayList<Integer> locationCells; 
+    ArrayList<Integer> locationCells;  
     ArrayList<Integer> guessedCells;   
 
-    public void setLocationCells(int[] locs) {
-        locationCells = new ArrayList<>();
-        for (int loc : locs) {
-            locationCells.add(loc);  
-        }
+ 
+    public void setLocationCells(ArrayList<Integer> locs) {
+        locationCells = locs;
         guessedCells = new ArrayList<>();  
     }
+
+
 
     public String checkYourself(String stringGuess) {
         int guess = Integer.parseInt(stringGuess);
         String result = "мимо";
-        if (guessedCells.contains(guess)) {
+        
+        if (!guessedCells.contains(guess)) {
+            guessedCells.add(guess);
+        }
+        else {
             return "Эта клетка уже была угадана";
         }
-        guessedCells.add(guess);  
+
         if (locationCells.contains(guess)) {
             numOfHits++;
-            locationCells.remove(Integer.valueOf(guess)); // Убираем угаданную клетку из списка
+            int index = locationCells.indexOf(guess);
+            locationCells.remove(index);  
             result = "попал";
         }
+
         if (locationCells.isEmpty()) {
-            result = "выиграл";
+            result = "потопил";
         }
         return result;
     }
+
+
     public static void main(String[] args) {
         GameHelper helper = new GameHelper();
         SimpleDotCom theDotCom = new SimpleDotCom();
+        
+        
+        ArrayList<Integer> locations = new ArrayList<>();
         int randomNum = (int) (Math.random() * 5);
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2}; // Генерация случайных координат
+        for (int i = randomNum; i < randomNum + 3; i++) {
+            locations.add(i);  
+        }
+        
         theDotCom.setLocationCells(locations);
+
         int numOfGuesses = 0;
         boolean isAlive = true;
+
         while (isAlive) {
             String guess = helper.getUserInput("введите число");
             String result = theDotCom.checkYourself(guess);
